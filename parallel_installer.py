9b7ed72eb09ca8a55f99ce1257b8482749a3e264
Jinjian Liu <leifmax@berkeley.edu>
2024-08-14 07:05:26 -0700

    install custom git

--- parallel_installer.py
+++ parallel_installer.py
@@ -24,7 +24,7 @@ def main():
             && pdm config venv.backend virtualenv \
             && pdm add -g setuptools==70.2.0 \
             && pdm add -g wheel \
-            && pip install pipreqs\
+            && pip install git+https://github.com/berkeleyljj/pipreqs_ljj.git\
         ",
     )
 
